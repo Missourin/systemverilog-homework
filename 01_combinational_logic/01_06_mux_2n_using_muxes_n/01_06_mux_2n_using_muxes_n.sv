4cc879197dd7cd11bcac64ff22b8c39549c17662
Missourin <208460828+Missourin@users.noreply.github.com>
2026-07-01 00:24:22 +0300

    6/10

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -27,5 +27,11 @@ module mux_4_1
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
 
+  wire [3:0] out_low;
+  wire [3:0] out_high;
 
+  mux_2_1 im_0_mux_2_1 (d0, d1, sel[0], out_low);
+  mux_2_1 im_1_mux_2_1 (d2, d3, sel[0], out_high);
+
+  mux_2_1 final_mux_2_1 (out_low, out_high, sel[1], y);
 endmodule
