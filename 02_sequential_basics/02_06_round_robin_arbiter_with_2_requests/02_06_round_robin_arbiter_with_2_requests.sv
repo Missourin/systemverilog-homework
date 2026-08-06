cf6e88cd7d8b34a4f2cdfcd5b260cfeb4ef95344
Missourin <208460828+Missourin@users.noreply.github.com>
2026-08-06 20:49:04 +0300

    sixth one is ready

--- 02_sequential_basics/02_06_round_robin_arbiter_with_2_requests/02_06_round_robin_arbiter_with_2_requests.sv
+++ 02_sequential_basics/02_06_round_robin_arbiter_with_2_requests/02_06_round_robin_arbiter_with_2_requests.sv
@@ -4,10 +4,10 @@
 
 module round_robin_arbiter_with_2_requests
 (
-    input        clk,
-    input        rst,
-    input  [1:0] requests,
-    output [1:0] grants
+    input              clk,
+    input              rst,
+    input        [1:0] requests,
+    output logic [1:0] grants
 );
     // Task:
     // Implement a "arbiter" module that accepts up to two requests
@@ -23,5 +23,32 @@ module round_robin_arbiter_with_2_requests
     // requests -> 01 00 10 11 11 00 11 00 11 11
     // grants   -> 01 00 10 01 10 00 01 00 10 01
 
+    logic priority_guest;
+
+    always_comb begin
+        grants = 2'b00;
+
+        case (requests)
+            2'b01: grants = 2'b01;
+            2'b10: grants = 2'b10;
+            2'b11: begin
+                if (!priority_guest) grants = 2'b01;
+                else                 grants = 2'b10;
+            end
+            default: grants = 2'b00;
+        endcase
+    end
+
+    always_ff @ (posedge clk)
+        if (rst)
+        begin
+            priority_guest <= '0;
+        end
+        else
+        begin
+            if      (grants == 2'b01) priority_guest <= '1;
+            else if (grants == 2'b10) priority_guest <= '0;
+            else                      priority_guest <= priority_guest;
+        end
 
 endmodule
