cd1b07889c3df09d4deee9d38bdf688cb81b5ba1
Missourin <208460828+Missourin@users.noreply.github.com>
2026-08-13 15:56:36 +0300

    seventh is ready

--- 02_sequential_basics/02_07_serial_to_parallel/02_07_serial_to_parallel.sv
+++ 02_sequential_basics/02_07_serial_to_parallel/02_07_serial_to_parallel.sv
@@ -27,17 +27,17 @@ module serial_to_parallel
     // Note:
     // Check the waveform diagram in the README for better understanding.
 
-    logic [width - 1:0    ]   register;
-    logic [$clog2(width):0]   counter;
-    logic                     last_bit;
+    logic [$clog2(width):0]  cnt;
+    logic [width - 1    :0]  shift_reg;
+    logic                    last_bit;
 
-    assign last_bit = serial_valid & (counter == width - 1);
+    assign last_bit = serial_valid & (cnt == width - 1);
 
-    always_ff @ (posedge clk) begin
+    always_ff @ (posedge clk)
         if (rst)
         begin
-            counter        <= '0;
-            register       <= '0;
+            cnt            <= '0;
+            shift_reg      <= '0;
             parallel_valid <= '0;
             parallel_data  <= '0;
         end
@@ -48,19 +48,18 @@ module serial_to_parallel
 
             if (serial_valid)
             begin
-                register <= { serial_data, register [width - 1:1] };
+                shift_reg <= { serial_data, shift_reg [width-1:1] };
 
                 if (last_bit)
                 begin
-                    counter        <= '0;
+                    cnt            <= '0;
                     parallel_valid <= '1;
-                    parallel_data  <= { serial_data, register [width - 1:1] };
+                    parallel_data  <= { serial_data, shift_reg [width-1:1] };
                 end
                 else
                 begin
-                    counter  <= counter + 1'b1;
+                    cnt <= cnt + 1'b1;
                 end
             end
         end
-    end
 endmodule
