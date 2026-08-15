aa44a5b1f94c001b30aad14a461cf824474d924e
Missourin <208460828+Missourin@users.noreply.github.com>
2026-08-16 02:46:35 +0300

    second one is ready

--- 03_combinational_arithmetic/03_02_signed_add_with_saturation/03_02_signed_add_with_saturation.sv
+++ 03_combinational_arithmetic/03_02_signed_add_with_saturation/03_02_signed_add_with_saturation.sv
@@ -18,8 +18,8 @@ endmodule
 
 module signed_add_with_saturation
 (
-  input  [3:0] a, b,
-  output [3:0] sum
+  input        [3:0] a, b,
+  output logic [3:0] sum
 );
 
   // Task:
@@ -36,5 +36,13 @@ module signed_add_with_saturation
   // and the arguments are negative,
   // the sum should be set to the minimum negative number.
 
+  always_comb begin
+    sum = a + b;
+
+    if ((a[3] == 1'b0) && (b[3] == 1'b0) && (sum[3] != a[3]))
+      sum = 4'b0111;
+    else if ((a[3] == 1'b1) && (b[3] == 1'b1) && (sum[3] != a[3]))
+      sum = 4'b1000;
+  end
 
 endmodule
