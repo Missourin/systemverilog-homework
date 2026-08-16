8b32d7c1c52dcc5a3173a47f0f16bbc26587c1ea
Missourin <208460828+Missourin@users.noreply.github.com>
2026-08-16 06:44:40 +0300

    seventh is ready

--- 03_combinational_arithmetic/03_07_sort_floats/03_07_sort_floats.sv
+++ 03_combinational_arithmetic/03_07_sort_floats/03_07_sort_floats.sv
@@ -86,5 +86,73 @@ module sort_three_floats (
     // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
     // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
 
+    wire  err1, err2, err3;
+    logic u0_less_or_equal_u1, u1_less_or_equal_u2, u0_less_or_equal_u2;
+
+    f_less_or_equal i_floe_1
+    (
+        .a   ( unsorted [0]        ),
+        .b   ( unsorted [1]        ),
+        .res ( u0_less_or_equal_u1 ),
+        .err ( err1                )
+    );
+
+    f_less_or_equal i_floe_2
+    (
+        .a   ( unsorted [1]        ),
+        .b   ( unsorted [2]        ),
+        .res ( u1_less_or_equal_u2 ),
+        .err ( err2                )
+    );
+
+    f_less_or_equal i_floe_3
+    (
+        .a   ( unsorted [0]        ),
+        .b   ( unsorted [2]        ),
+        .res ( u0_less_or_equal_u2 ),
+        .err ( err3                )
+    );
+
+    assign err = err1 | err2 | err3;
+
+    always_comb begin
+        if (u0_less_or_equal_u1)
+        begin
+            if (u1_less_or_equal_u2)
+            begin
+                sorted = unsorted;
+            end
+            else
+            begin
+                if (u0_less_or_equal_u2)
+                begin
+                    { sorted [0], sorted [1], sorted [2] } = { unsorted [0], unsorted [2], unsorted [1] };
+                end
+                else
+                begin
+                    { sorted [0], sorted [1], sorted [2] } = { unsorted [2], unsorted [0], unsorted [1] };
+                end
+            end
+        end
+
+        else
+        begin
+            if (u1_less_or_equal_u2)
+            begin
+                if (u0_less_or_equal_u2)
+                begin
+                    { sorted [0], sorted [1], sorted [2] } = { unsorted [1], unsorted [0], unsorted [2] };
+                end
+                else
+                begin
+                    { sorted [0], sorted [1], sorted [2] } = { unsorted [1], unsorted [2], unsorted [0] };
+                end
+            end
+            else
+            begin
+                { sorted [0], sorted [1], sorted [2] } = { unsorted [2], unsorted [1], unsorted [0] };
+            end
+        end
+    end
 
 endmodule
