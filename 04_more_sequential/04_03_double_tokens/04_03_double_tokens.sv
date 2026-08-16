96eee42fab263fd445af229999c6e1316b8c4882
Missourin <208460828+Missourin@users.noreply.github.com>
2026-08-16 16:20:13 +0300

    fourth one is ready

--- 04_more_sequential/04_03_double_tokens/04_03_double_tokens.sv
+++ 04_more_sequential/04_03_double_tokens/04_03_double_tokens.sv
@@ -25,5 +25,38 @@ module double_tokens
     // a -> 10010011000110100001100100
     // b -> 11011011110111111001111110
 
+    logic [7:0] in_ones_cnt;
+    logic [8:0] out_gen_cnt;
+
+    assign b = (!overflow & (out_gen_cnt > '0 | a)) ? 1'b1 : 1'b0;
+
+    always_ff @ (posedge clk) begin
+        if (rst) begin
+            in_ones_cnt <= '0;
+            out_gen_cnt <= '0;
+            overflow    <= '0;
+        end
+        else begin
+            if (a) begin
+                in_ones_cnt <= in_ones_cnt + 1'b1;
+
+                if (in_ones_cnt >= 8'd200)
+                    overflow <= 1'b1;
+            end
+            else begin
+                in_ones_cnt <= '0;
+            end
+
+            if (a & b) begin
+                out_gen_cnt <= out_gen_cnt + 1'b1;
+            end
+            else if (a) begin
+                out_gen_cnt <= out_gen_cnt + 2'd2;
+            end
+            else if (b) begin
+                out_gen_cnt <= out_gen_cnt - 1'b1;
+            end
+        end
+    end
 
 endmodule
