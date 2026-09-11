2a3dd5fa46e7603cf39abccf4923f5d5103ddd62
Missourin <208460828+Missourin@users.noreply.github.com>
2026-09-11 13:47:20 +0300

    fifth one is ready

--- 05_finite_state_machines/05_04_05_sqrt_formula_fsms/05_05_formula_2_fsm.sv
+++ 05_finite_state_machines/05_04_05_sqrt_formula_fsms/05_05_formula_2_fsm.sv
@@ -34,5 +34,39 @@ module formula_2_fsm
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
 
+    enum logic [1:0]
+    {
+        IDLE     = 2'b00,
+        WAIT_C   = 2'b01,
+        WAIT_BC  = 2'b11,
+        WAIT_ABC = 2'b10
+    }
+    state, new_state;
 
+    always_comb begin
+        new_state = state;
+
+        case (state)
+            IDLE     : if (arg_vld)     new_state = WAIT_C;
+            WAIT_C   : if (isqrt_y_vld) new_state = WAIT_BC;
+            WAIT_BC  : if (isqrt_y_vld) new_state = WAIT_ABC;
+            WAIT_ABC : if (isqrt_y_vld) new_state = IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clk)
+        if (rst) state <= IDLE;
+        else     state <= new_state;
+
+    assign isqrt_x_vld = ((state == IDLE) && arg_vld) ||
+                         (isqrt_y_vld && ((state == WAIT_C) || (state == WAIT_BC)));
+
+    always_comb
+        if      (state == IDLE   ) isqrt_x = c;
+        else if (state == WAIT_C ) isqrt_x = (b + { 16'b0, isqrt_y });
+        else if (state == WAIT_BC) isqrt_x = (a + { 16'b0, isqrt_y });
+        else                       isqrt_x = '0;
+
+    assign res_vld = ((state == WAIT_ABC) && isqrt_y_vld);
+    assign res = { 16'b0, isqrt_y };
 endmodule
