c0c00eed3ac226699bfa017b358afa0b538a3265
Missourin <208460828+Missourin@users.noreply.github.com>
2026-09-11 12:54:07 +0300

    fourth one is ready

--- 05_finite_state_machines/05_04_05_sqrt_formula_fsms/05_04_formula_1_impl_2_fsm.sv
+++ 05_finite_state_machines/05_04_05_sqrt_formula_fsms/05_04_formula_1_impl_2_fsm.sv
@@ -41,5 +41,45 @@ module formula_1_impl_2_fsm
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm
 
+    enum logic [1:0]
+    {
+        IDLE    = 2'b00,
+        WAIT_AB = 2'b01,
+        WAIT_C  = 2'b11
+    }
+    state, new_state;
+
+    logic [31:0] sum_ab;
+
+    always_comb begin
+        new_state = state;
+
+        case (state)
+            IDLE    : if (arg_vld)                       new_state = WAIT_AB;
+            WAIT_AB : if (isqrt_1_y_vld & isqrt_2_y_vld) new_state = WAIT_C;
+            WAIT_C  : if (isqrt_1_y_vld)                 new_state = IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clk)
+        if (rst) state <= IDLE;
+        else     state <= new_state;
+
+    assign isqrt_1_x_vld = (state == IDLE && arg_vld) ||
+                           ((state == WAIT_AB) && isqrt_1_y_vld && isqrt_2_y_vld);
+
+    assign isqrt_2_x_vld = ((state == IDLE) && arg_vld);
+
+    assign isqrt_1_x = (state == IDLE) ? a : c;
+    assign isqrt_2_x = b;
+
+    always_ff @(posedge clk)
+        if (rst)
+            sum_ab <= '0;
+        else if ((state == WAIT_AB) && isqrt_1_y_vld && isqrt_2_y_vld)
+            sum_ab <= { 16'b0, isqrt_1_y } + { 16'b0, isqrt_2_y };
+
+    assign res_vld = ((state == WAIT_C) && isqrt_1_y_vld);
+    assign res = sum_ab + { 16'b0, isqrt_1_y };
 
 endmodule
