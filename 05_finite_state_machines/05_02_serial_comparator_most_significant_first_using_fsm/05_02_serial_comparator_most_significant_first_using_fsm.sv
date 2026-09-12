f4cc31fcab76f418d03f766cf916b3fe1b76c70f
Missourin <208460828+Missourin@users.noreply.github.com>
2026-09-12 13:24:00 +0300

    corrected version

--- 05_finite_state_machines/05_02_serial_comparator_most_significant_first_using_fsm/05_02_serial_comparator_most_significant_first_using_fsm.sv
+++ 05_finite_state_machines/05_02_serial_comparator_most_significant_first_using_fsm/05_02_serial_comparator_most_significant_first_using_fsm.sv
@@ -73,24 +73,24 @@ module serial_comparator_most_significant_first_using_fsm
 
   enum logic [2:0]
   {
-    st_a_less_b    = 3'b100,
-    st_equal       = 3'b010,
-    st_a_greater_b = 3'b001
+    ST_A_LESS_B    = 3'b100,
+    ST_EQUAL       = 3'b010,
+    ST_A_GREATER_B = 3'b001
   }
   state, new_state;
 
   always_comb begin
     new_state = state;
 
-    if (state == st_equal)
-      if      (~a &  b) new_state = st_a_less_b;
-      else if ( a & ~b) new_state = st_a_greater_b;
+    if (state == ST_EQUAL)
+      if      (~a &  b) new_state = ST_A_LESS_B;
+      else if ( a & ~b) new_state = ST_A_GREATER_B;
   end
 
   assign { a_less_b, a_eq_b, a_greater_b } = new_state;
 
   always_ff @(posedge clk)
-    if (rst) state <= st_equal;
+    if (rst) state <= ST_EQUAL;
     else     state <= new_state;
 
 endmodule
